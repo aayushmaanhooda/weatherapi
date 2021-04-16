7b2347613fe920f1ebb3d02fb623f01cccfb3cf3
aayushmaan <aayushmaan162@gmail.com>
2021-04-16 16:46:08 +0530

    changes

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 import environ
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +127,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
